184cd6091a9077b9dcc1d4d206cdc1bf97b9e706
Vishal Kumar <vishalmicrosoft02@gmail.com>
2025-12-01 12:28:45 +0530

    Update Type Casting.py

--- Day 3/Type Casting.py
+++ Day 3/Type Casting.py
@@ -1,18 +1,16 @@
 #Typecasting in Python : The conversion of one data typ into the other data type is known as type casting in python
 a = '1'
 b = '2'
-print(a + b)#output : 12, because input of a and b is string  type
-a = 1
+print(a + b)
 b = 2
-print(a + b)#output : 3, because input of a and b is integer type
-#Explicit TypeCasting:
+print(a + b)
 a = '1'
 b = '2'
-print( int(a) + int(b) ) # converts string input to integer
-#Implicit TypeCasting:
+print( int(a) + int(b) ) 
 a = 1.9
 b = 8
-print(a + b)# agar ek input float hoga aur dusra integer to output float me hi ayega to prevent data loss.
+print(a + b)
+
 
 
  
